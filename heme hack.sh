b544780f98639079bfe6adb77cdcf155d7bd331c
Master-hack <96709855+DRACULA-HACK@users.noreply.github.com>
2022-05-15 18:37:54 +0530

    Create heme hack.sh

---
+++ heme hack.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+clear
+apt update
+apt install figlet
+clear
+figlet MASTER-HACK
+echo ""
+figlet heme hack
+echo ""
+sleep 15
+clear
+figlet this
+sleep 5
+clear
+figlet script
+sleep 5
+clear
+figlet is
+sleep 5
+clear
+figlet made
+sleep 5
+clear
+figlet by
+sleep 5
+clear
+figlet master-hack
+sleep 5
+
+echo "HEHE"
+sleep 5
+echo ""
+echo ""
+echo "what is your name ? "
+echo "{type  your name to continue}"
+echo " name:"
+read name
+echo "how old are you "
+echo "{type your age to continue}"
+echo "age:"
+read age
+sleep 3
+echo  "     "
+sleep 3
+echo " Hello $name you are $age years old hamm,,,"
+echo ""
+sleep 15
+
+echo "hello $name your device is going to hack by hacker"
+echo  " "
+
+
+echo "loading........."
+sleep 4
+echo "installing files...."
+sleep 10
+
+echo "shearing your Data "
+sleep 10
+clear
+echo "hacked the Data heme "
+echo ""
+sleep 15
+
+figlet HACKED
+sleep 10
+
+figlet HEME
+sleep 15
+echo ""
+clear
+echo " naw your device is going to crack hehe"
+sleep 15
+clear
+echo "1"
+sleep 9
+echo "2"
+sleep 9
+echo "3"
+sleep 9
+echo "4"
+sleep 9
+echo "5"
+sleep 5
+echo "loading..."
+sleep 10
+clear
+apt install cmatrix
+clear
+cmatrix
